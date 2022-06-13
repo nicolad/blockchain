ad61c78cf8f6b89e7ed53267e803d0b03b99e825
vadim.nicolai-EXT <vadim.nicolai-EXT@vitrifi.net>
2022-06-14 00:22:07 +0300

    Working version.

--- database/block.go
+++ database/block.go
@@ -8,21 +8,6 @@ import (
 
 type Hash [32]byte
 
-type Block struct {
-	Header BlockHeader
-	TXs    []Tx // new transactions only(payload)
-}
-
-type BlockHeader struct {
-	Parent Hash   // parent block hash reference
-	Time   uint64 // timestamp of block creation (in seconds)
-}
-
-type BlockFS struct {
-	Key   Hash  `json:"hash"`
-	Value Block `json:"block"`
-}
-
 func (h Hash) MarshalText() ([]byte, error) {
 	return []byte(hex.EncodeToString(h[:])), nil
 }
@@ -32,6 +17,25 @@ func (h *Hash) UnmarshalText(data []byte) error {
 	return err
 }
 
+type Block struct {
+	Header BlockHeader `json:"header"`
+	TXs    []Tx        `json:"payload"`
+}
+
+type BlockHeader struct {
+	Parent Hash   `json:"parent"`
+	Time   uint64 `json:"time"`
+}
+
+type BlockFS struct {
+	Key   Hash  `json:"hash"`
+	Value Block `json:"block"`
+}
+
+func NewBlock(parent Hash, time uint64, txs []Tx) Block {
+	return Block{BlockHeader{parent, time}, txs}
+}
+
 func (b Block) Hash() (Hash, error) {
 	blockJson, err := json.Marshal(b)
 	if err != nil {
@@ -39,8 +43,4 @@ func (b Block) Hash() (Hash, error) {
 	}
 
 	return sha256.Sum256(blockJson), nil
-}
-
-func NewBlock(parent Hash, time uint64, txs []Tx) Block {
-	return Block{BlockHeader{parent, time}, txs}
 }
--- database/state.go
+++ database/state.go
@@ -2,10 +2,8 @@ package database
 
 import (
 	"bufio"
-	"crypto/sha256"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"path/filepath"
 	"time"
@@ -35,36 +33,53 @@ func NewStateFromDisk() (*State, error) {
 		balances[account] = balance
 	}
 
-	f, err := os.OpenFile(filepath.Join(cwd, "database", "tx.db"), os.O_APPEND|os.O_RDWR, 0600)
+	f, err := os.OpenFile(filepath.Join(cwd, "database", "block.db"), os.O_APPEND|os.O_RDWR, 0600)
 	if err != nil {
 		return nil, err
 	}
 
 	scanner := bufio.NewScanner(f)
 
-	state := &State{balances, make([]Tx, 0), f, Snapshot{}}
+	state := &State{balances, make([]Tx, 0), f, Hash{}}
 
 	for scanner.Scan() {
 		if err := scanner.Err(); err != nil {
 			return nil, err
 		}
 
-		var tx Tx
-		json.Unmarshal(scanner.Bytes(), &tx)
-
-		if err := state.apply(tx); err != nil {
+		blockFsJson := scanner.Bytes()
+		var blockFs BlockFS
+		err = json.Unmarshal(blockFsJson, &blockFs)
+		if err != nil {
 			return nil, err
 		}
+
+		err = state.applyBlock(blockFs.Value)
+		if err != nil {
+			return nil, err
+		}
+
+		state.latestBlockHash = blockFs.Key
 	}
 
-	err = state.doSnapshot()
-	if err != nil {
-		return nil, err
-	}
 	return state, nil
 }
 
-func (s *State) Add(tx Tx) error {
+func (s *State) LatestBlockHash() Hash {
+	return s.latestBlockHash
+}
+
+func (s *State) AddBlock(b Block) error {
+	for _, tx := range b.TXs {
+		if err := s.AddTx(tx); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (s *State) AddTx(tx Tx) error {
 	if err := s.apply(tx); err != nil {
 		return err
 	}
@@ -74,24 +89,45 @@ func (s *State) Add(tx Tx) error {
 	return nil
 }
 
-func (s *State) LatestSnapshot() Hash {
-	return s.latestBlockHash
-}
-
 func (s *State) Persist() (Hash, error) {
-
 	block := NewBlock(s.latestBlockHash, uint64(time.Now().Unix()), s.txMempool)
 	blockHash, err := block.Hash()
 	if err != nil {
 		return Hash{}, err
 	}
+
 	blockFs := BlockFS{blockHash, block}
 
-	return s.snapshot, nil
+	blockFsJson, err := json.Marshal(blockFs)
+	if err != nil {
+		return Hash{}, err
+	}
+
+	fmt.Printf("Persisting new Block to disk:\n")
+	fmt.Printf("\t%s\n", blockFsJson)
+
+	if _, err = s.dbFile.Write(append(blockFsJson, '\n')); err != nil {
+		return Hash{}, err
+	}
+	s.latestBlockHash = blockHash
+
+	s.txMempool = []Tx{}
+
+	return s.latestBlockHash, nil
 }
 
-func (s *State) Close() {
-	s.dbFile.Close()
+func (s *State) Close() error {
+	return s.dbFile.Close()
+}
+
+func (s *State) applyBlock(b Block) error {
+	for _, tx := range b.TXs {
+		if err := s.apply(tx); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 func (s *State) apply(tx Tx) error {
@@ -100,26 +136,12 @@ func (s *State) apply(tx Tx) error {
 		return nil
 	}
 
-	if s.Balances[tx.From]-tx.Value < 0 {
+	if s.Balances[tx.From] < tx.Value {
 		return fmt.Errorf("insufficient balance")
 	}
 
 	s.Balances[tx.From] -= tx.Value
 	s.Balances[tx.To] += tx.Value
 
-	return nil
-}
-
-func (s *State) doSnapshot() error {
-	// Re-read the whole file from the first byte
-	_, err := s.dbFile.Seek(0, 0)
-	if err != nil {
-		return err
-	}
-	txsData, err := ioutil.ReadAll(s.dbFile)
-	if err != nil {
-		return err
-	}
-	s.snapshot = sha256.Sum256(txsData)
 	return nil
 }
